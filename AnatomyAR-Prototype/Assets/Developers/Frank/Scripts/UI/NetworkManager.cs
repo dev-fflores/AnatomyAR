bbfcbbbc3a4ca1c86dc6b01b940f1e3e444ca1f5
Frank Flores Rutti <ffloresrutti@gmail.com>
2021-06-14 22:29:21 -0500

    Update

--- AnatomyAR-Prototype/Assets/Developers/Frank/Scripts/UI/NetworkManager.cs
+++ AnatomyAR-Prototype/Assets/Developers/Frank/Scripts/UI/NetworkManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class NetworkManager : MonoBehaviour
 {
@@ -16,11 +17,23 @@ public class NetworkManager : MonoBehaviour
         _form.AddField("email", email);
         _form.AddField("pass", pass);
 
-        WWW _w = new WWW("http://localhost/Anatomy_AR/createUser.php", _form);
+        //WWW _w = new WWW("http://localhost/Anatomy_AR/php/createUser.php", _form);
+        
+        //WWW _w = new WWW("http://anatomyar.great-site.net/createUser.php", _form);
 
-        yield return _w;
+        UnityWebRequest _www = UnityWebRequest.Post("http://anatomyar.great-site.net/createUser.php", _form);
 
-        response(JsonUtility.FromJson<Response>(_w.text));
+        yield return _www.SendWebRequest();
+        Debug.Log(_www.downloadHandler.text);
+
+        /*if (_www.isNetworkError || _www.isHttpError) {
+
+            Debug.LogError(_www.error);
+
+        } else {
+            Debug.Log(_www.downloadHandler.text);
+            //response(JsonUtility.FromJson<Response>(_www.downloadHandler.text));
+        }*/
     }
 
     public void checkUser(string userName, string pass, Action<Response> response) {
@@ -32,11 +45,22 @@ public class NetworkManager : MonoBehaviour
         _form.AddField("userName", userName);
         _form.AddField("pass", pass);
 
-        WWW _w = new WWW("http://localhost/Anatomy_AR/checkUser.php", _form);
+        //WWW _w = new WWW("http://localhost/Anatomy_AR/checkUser.php", _form);
+        //WWW _w = new WWW("http://anatomyar.great-site.net/checkUser.php", _form);
+        UnityWebRequest _www = UnityWebRequest.Get("http://anatomyar.great-site.net/checkUser.php");
 
-        yield return _w;
-        Debug.Log(_w.text);
-        response(JsonUtility.FromJson<Response>(_w.text));
+        yield return _www.SendWebRequest();
+        Debug.Log(_www.downloadHandler.text);
+
+        /*if (_www.isNetworkError || _www.isHttpError) {
+
+            Debug.LogError(_www.error);
+
+        } else {
+            Debug.Log(_www.downloadHandler.text);
+            //response(JsonUtility.FromJson<Response>(_www.downloadHandler.text));
+            
+        }*/
     }
 
 }
